9bbb4352673d8910a9e9cbc759fc9eb49be563d8
MichaelHabermas <michaelghabermas@gmail.com>
2021-05-03 15:09:02 -0400

    new works

--- oldmaid/src/index.js
+++ oldmaid/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './cssFiles/index.css';
+import App2 from './components/App2';
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
