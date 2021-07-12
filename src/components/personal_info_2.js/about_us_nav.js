821e815cc4a59dfd391164f4fb6a74643f4284a2
William Moon <william-moon@lambdastudents.com>
2021-07-12 16:40:10 -0700

    new about pages boiler plate

---
+++ src/components/personal_info_2.js/about_us_nav.js
@@ -0,0 +1,31 @@
+import react from 'react';
+
+import styled from 'styled-components';
+
+const NavBar = styled.div`
+
+`
+
+const Nav = (props) => {
+    const { name, cvLink } = props
+
+
+    return (
+        <NavBar>
+            <nav>
+                <div className="nameLogo">{name}</div>
+                <ul>
+                    <li>
+                        <a href={cvLink} target="_blank" rel="noreferrer">CV</a>
+                    </li>
+                    <li>
+                        <a href="/" target="_blank" rel="noreferrer">CV</a>
+                    </li>
+                </ul>
+            </nav>
+
+        </NavBar>
+    )
+}
+
+export default Nav;
---
+++ src/components/personal_info_2.js/michael_about.js
@@ -0,0 +1,17 @@
+import react from 'react';
+import styled from 'styled-components';
+
+const MichaelMain = styled.div`
+
+`
+
+const MichaelMainAbout = () => {
+
+
+    return (
+        <MichaelMain>
+            <Nav />
+
+        </MichaelMain>
+    )
+}
