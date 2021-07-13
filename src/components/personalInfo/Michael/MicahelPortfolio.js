24a68c3d59140e6053ed0f5178b73b8c02add855
MichaelHabermas <michaelghabermas@gmail.com>
2021-07-12 20:13:32 -0400

    initial setup

---
+++ src/components/personalInfo/Michael/MicahelPortfolio.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const MichaelPortfolioStyles = styled`
+    color: red;
+`;
+
+function MichaelPortfolio() {
+	return (
+		<MichaelPortfolioStyles>
+			<h1> Hi, I'm Michael</h1>
+		</MichaelPortfolioStyles>
+	);
+}
+
+export default MichaelPortfolio;
