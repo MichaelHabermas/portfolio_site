93eb8fc6ef4ed8db746c1accfa915f58cdedd96b
MichaelHabermas <michaelghabermas@gmail.com>
2021-07-15 20:27:53 -0400

    proj icon tests added

--- src/assets/projects/index.js
+++ src/assets/projects/index.js
@@ -1,22 +1,21 @@
-import React from "react";
-
 import recipeazy from "../michaelPage/p1_recipeazy.png";
 
 export const michaelsProjects = [
 	{
 		image: recipeazy,
 		text: "Contributed to the design and implementation of a React app that gathers and shares family recipes. This app uses Redux to manage state, and makes axios calls to our concurrent team’s Node API.",
-		icons: [1, 2, 3],
+		icons: ["Website", "GitHub", "CSS3", "Javascript", "React"],
 	},
 	{
 		image: recipeazy,
 		text: "Contributed to the design and implementation of a React app that gathers and shares family recipes. This app uses Redux to manage state, and makes axios calls to our concurrent team’s Node API.",
-		icons: [1, 2, 3],
+		icons: ["Website", "GitHub", "CSS3", "Javascript", "React"],
 	},
 	{
 		image: recipeazy,
 		text: "Contributed to the design and implementation of a React app that gathers and shares family recipes. This app uses Redux to manage state, and makes axios calls to our concurrent team’s Node API.",
-		icons: [1, 2, 3],
+		icons: ["Website", "GitHub", "CSS3", "Javascript", "React"],
 	},
 ];
+
 export const willsProjects = [];
