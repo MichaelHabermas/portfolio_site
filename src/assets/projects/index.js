58e9aa08842cd10bde7ef84d607ed4f35366c20c
MichaelHabermas <michaelghabermas@gmail.com>
2021-07-15 20:56:54 -0400

    cleanup

--- src/assets/projects/index.js
+++ src/assets/projects/index.js
@@ -16,6 +16,52 @@ export const michaelsProjects = [
 		text: "Contributed to the design and implementation of a React app that gathers and shares family recipes. This app uses Redux to manage state, and makes axios calls to our concurrent team’s Node API.",
 		icons: ["Website", "GitHub", "CSS3", "Javascript", "React"],
 	},
+	{
+		image: recipeazy,
+		text: "Contributed to the design and implementation of a React app that gathers and shares family recipes. This app uses Redux to manage state, and makes axios calls to our concurrent team’s Node API.",
+		icons: ["Website", "GitHub", "CSS3", "Javascript", "React"],
+	},
+	{
+		image: recipeazy,
+		text: "Contributed to the design and implementation of a React app that gathers and shares family recipes. This app uses Redux to manage state, and makes axios calls to our concurrent team’s Node API.",
+		icons: ["Website", "GitHub", "CSS3", "Javascript", "React"],
+	},
+	{
+		image: recipeazy,
+		text: "Contributed to the design and implementation of a React app that gathers and shares family recipes. This app uses Redux to manage state, and makes axios calls to our concurrent team’s Node API.",
+		icons: ["Website", "GitHub", "CSS3", "Javascript", "React"],
+	},
 ];
 
-export const willsProjects = [];
+export const willsProjects = [
+	{
+		image: recipeazy,
+		text: "Contributed to the design and implementation of a React app that gathers and shares family recipes. This app uses Redux to manage state, and makes axios calls to our concurrent team’s Node API.",
+		icons: ["Website", "GitHub", "CSS3", "Javascript", "React"],
+	},
+	{
+		image: recipeazy,
+		text: "Contributed to the design and implementation of a React app that gathers and shares family recipes. This app uses Redux to manage state, and makes axios calls to our concurrent team’s Node API.",
+		icons: ["Website", "GitHub", "CSS3", "Javascript", "React"],
+	},
+	{
+		image: recipeazy,
+		text: "Contributed to the design and implementation of a React app that gathers and shares family recipes. This app uses Redux to manage state, and makes axios calls to our concurrent team’s Node API.",
+		icons: ["Website", "GitHub", "CSS3", "Javascript", "React"],
+	},
+	{
+		image: recipeazy,
+		text: "Contributed to the design and implementation of a React app that gathers and shares family recipes. This app uses Redux to manage state, and makes axios calls to our concurrent team’s Node API.",
+		icons: ["Website", "GitHub", "CSS3", "Javascript", "React"],
+	},
+	{
+		image: recipeazy,
+		text: "Contributed to the design and implementation of a React app that gathers and shares family recipes. This app uses Redux to manage state, and makes axios calls to our concurrent team’s Node API.",
+		icons: ["Website", "GitHub", "CSS3", "Javascript", "React"],
+	},
+	{
+		image: recipeazy,
+		text: "Contributed to the design and implementation of a React app that gathers and shares family recipes. This app uses Redux to manage state, and makes axios calls to our concurrent team’s Node API.",
+		icons: ["Website", "GitHub", "CSS3", "Javascript", "React"],
+	},
+];
