5fb9bf9c9a1ef79df9ebf6ecca8c27872b56bcf2
MichaelHabermas <michaelghabermas@gmail.com>
2021-07-19 18:36:33 -0400

    sizing 5

--- src/components/personalInfo/Michael/MichaelPortfolio.js
+++ src/components/personalInfo/Michael/MichaelPortfolio.js
@@ -80,7 +80,7 @@ const MichaelPortfolioStyles = styled.div`
 		-webkit-background-size: cover;
 		-moz-background-size: cover;
 		-o-background-size: cover;
-		background-size: 100% 100%;
+		background-size: 100% auto;
 		height: 95vh;
 		color: white;
 	}
